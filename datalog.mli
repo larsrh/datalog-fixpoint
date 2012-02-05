2d85e9833952ed4d64cf23ff3ac5bd564a623690
Lars Hupel <lars.hupel@mytum.de>
2012-02-05 01:26:55 +0100

    datalog module type

---
+++ datalog.mli
@@ -0,0 +1,25 @@
+open Util
+
+type var = string
+
+type relation = string
+
+type 'a symbol = {
+	rel: relation;
+	params: ('a, var) either list;
+}
+
+module type Datalog = sig
+	type number
+	type 'a constr
+
+	type clause = {
+		head: number symbol;
+		syms: number symbol list;
+		constraints: number constr list;
+	}
+
+	val fixpoint: clause list -> clause list
+
+	val contained: clause list -> relation -> number list -> bool
+end;;
