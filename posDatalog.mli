5e3a3753ede5fd32e6747a18119aad52aa86d36e
Lars Hupel <lars.hupel@mytum.de>
2012-03-25 23:33:59 +0200

    documentation: PosDatalog, parts of Unification

--- posDatalog.mli
+++ posDatalog.mli
@@ -1,10 +1,22 @@
+(** {!Datalog} instance for positive Datalog. *)
+
 open Datalog
 open Types
 
 include Interface with type number = int
 
+(** Create a positive constraint. The result will be [None] iff, after
+    simplification, a coefficient is less than zero.
+
+    Example: [mkPosConstraint [1, "x"; 2, "y"] true 2] translates to
+    [x+2y >= 2] *)
 val mkPosConstraint: (int * var) list -> bool -> int -> number constr result option
+
+(** Create an upper bound constraint. [mkUpperBound v incl b] means [v < b] iff
+    [incl] is false, otherwise [v <= b].*)
 val mkUpperBound: var -> bool -> int -> number constr
 
+
+(**/**)
 
 val test: OUnit.test
--- unification.mli
+++ unification.mli
@@ -1,6 +1,11 @@
+(** Limited kind of unification, where only {{!Types.exp}simple expressions}
+    are allowed. *)
+
 open Types
 open Util
 
+(** Generates a fresh identifier. This function has {e no} side-effects. It is
+    guaranteed that [freshVar s] is not a member of [s]. *)
 val freshVar: stringSet -> var
 
 val unify: 'a exp list -> 'a exp list -> 'a mapping option
@@ -9,5 +14,7 @@ val mergeEqualities: 'a equality list list -> 'a equality list option
 
 val canonicalizeVars: 'a equality list -> 'a exp stringMap
 
+
+(**/**)
 
 val test: OUnit.test
