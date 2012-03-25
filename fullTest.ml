b9a9aa7577da955eef3736f97b46f37cf4a8d9df
Lars Hupel <lars.hupel@mytum.de>
2012-03-25 17:41:50 +0200

    tuned `FullTest'

--- fullTest.ml
+++ fullTest.ml
@@ -7,21 +7,18 @@ open Util
 let test =
 	let open OUnit in
 
-	let fullCases =
-		let root = "tests/pos" in
-		let f folder () =
-			let toSet list = fold_right StringSet.add list StringSet.empty in
-			let fixpoint =
-				readFile (folder ^ Filename.dir_sep ^ "input") |>
-				String.concat " " |>
-				Lexer.parseString |>
-				PosDatalog.fixpoint |>
-				map PosDatalog.showClause in
-			let output = readFile (folder ^ Filename.dir_sep ^ "output") in
-			assert_equal (length fixpoint) (length output);
-			assert_equal ~cmp:StringSet.equal (toSet output) (toSet fixpoint) in
-		let case folder = folder >:: f (root ^ Filename.dir_sep ^ folder) in
-		Sys.readdir root |> Array.to_list |> map case
-	in
-
-	"FullTest" >::: fullCases
+	let root = "tests/pos" in
+	let f folder () =
+		let toSet list = fold_right StringSet.add list StringSet.empty in
+		let fixpoint =
+			readFile (folder ^ Filename.dir_sep ^ "input") |>
+			String.concat " " |>
+			Lexer.parseString |>
+			PosDatalog.fixpoint |>
+			map PosDatalog.showClause in
+		let output = readFile (folder ^ Filename.dir_sep ^ "output") in
+		assert_equal (length fixpoint) (length output);
+		assert_equal ~cmp:StringSet.equal (toSet output) (toSet fixpoint) in
+	let case folder = folder >:: f (root ^ Filename.dir_sep ^ folder) in
+	let cases = Sys.readdir root |> Array.to_list |> map case in
+	"FullTest" >::: cases
