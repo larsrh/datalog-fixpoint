b9c83cf9633f733d1369c3ea55cf2e98d654401a
Lars Hupel <lars.hupel@mytum.de>
2012-05-16 15:49:48 +0200

    check whether the fixpoint is actually a fixpoint in fullTest

--- fullTest.ml
+++ fullTest.ml
@@ -8,17 +8,28 @@ let test =
 	let open OUnit in
 
 	let root = "tests/pos" in
-	let f folder () =
+
+	let f folder =
 		let toSet list = fold_right StringSet.add list StringSet.empty in
-		let fixpoint =
+		let parsed =
 			readFile (folder ^ Filename.dir_sep ^ "input") |>
 			String.concat " " |>
-			Lexer.parseString |>
-			PosDatalog.fixpoint |>
-			map PosDatalog.showClause in
+			Lexer.parseString in
+		let fixpoint = PosDatalog.fixpoint parsed in
 		let output = readFile (folder ^ Filename.dir_sep ^ "output") in
-		assert_equal (length fixpoint) (length output);
-		assert_equal ~cmp:StringSet.equal (toSet output) (toSet fixpoint) in
-	let case folder = folder >:: f (root ^ Filename.dir_sep ^ folder) in
+
+		let firstIteration () =
+			assert_equal (length fixpoint) (length output);
+			assert_equal ~cmp:StringSet.equal (toSet output) (map PosDatalog.showClause fixpoint |> toSet) in
+
+		let secondIteration () =
+			assert_equal fixpoint (PosDatalog.fixpoint fixpoint) in
+
+		[
+			"firstIteration" >:: firstIteration;
+			"secondIteration" >:: secondIteration
+		] in
+
+	let case folder = folder >::: f (root ^ Filename.dir_sep ^ folder) in
 	let cases = Sys.readdir root |> Array.to_list |> map case in
 	"FullTest" >::: cases
